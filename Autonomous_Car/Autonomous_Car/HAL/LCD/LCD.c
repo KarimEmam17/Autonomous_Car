24e2b5baa9f9936a3e6f891c694eb16426d140c4
Karim  Mohamed Emam <104641970+KarimEmam17@users.noreply.github.com>
2022-06-24 22:22:18 +0200

    first  change LCD

--- Autonomous_Car/Autonomous_Car/HAL/LCD/LCD.c
+++ Autonomous_Car/Autonomous_Car/HAL/LCD/LCD.c
@@ -1,156 +1,118 @@
+
 /*
  * LCD.c
  *
- * Created: 5/27/2022 4:41:34 PM
- *  Author: K.EMAM
+ * Author : Hossam Elbahrawy
  */
 #include "LCD.h"
-#define F_CPU 16000000
-#include <util/delay.h>
-#include <math.h>
 
-void LCD_INIT(void)
+/*
+*Function name 	: lcd_init
+*Parameters	    : void
+*return		   		: void
+*purpose       	: initialize LCD pins as output
+*				 		and seting up the operation mode(4-bit)
+*						default setting (cursor on)
+*/
+void lcd_init(void)
 {
-	/*Set direction of command pins (RS, RW, E)*/
-	GPIO_SetPinDir(LCD_4_BIT_CMD_PORT, RS_PIN, GPIO_PIN_OUTPUT);
-	GPIO_SetPinDir(LCD_4_BIT_CMD_PORT, RW_PIN, GPIO_PIN_OUTPUT);
-	GPIO_SetPinDir(LCD_4_BIT_CMD_PORT, E_PIN , GPIO_PIN_OUTPUT);
-	/*Set direction of data pins*/
-	GPIO_SetPinDir(LCD_4_BIT_DATA_PORT, LCD_PIN4, GPIO_PIN_OUTPUT);
-	GPIO_SetPinDir(LCD_4_BIT_DATA_PORT, LCD_PIN5, GPIO_PIN_OUTPUT);
-	GPIO_SetPinDir(LCD_4_BIT_DATA_PORT, LCD_PIN6, GPIO_PIN_OUTPUT);
-	GPIO_SetPinDir(LCD_4_BIT_DATA_PORT, LCD_PIN7, GPIO_PIN_OUTPUT);
-	_delay_ms(100);
-	/*choose 4-bit mode*/
-	LCD_WriteCMD(0x33);
-	LCD_WriteCMD(0x32);
-	LCD_WriteCMD(LCD_4_Bit_Mode);
-	///////////////////////////
-	LCD_WriteCMD(Clear_Display_Screen);
-	LCD_WriteCMD(Return_Home);
-	LCD_WriteCMD(Shift_cursor_Right_Per_Read_Write);
-	LCD_WriteCMD(Display_ON_Cursor_OFF);
-	
+
+	DATA_DDR = (1<<LCD_D7) | (1<<LCD_D6) | (1<<LCD_D5)| (1<<LCD_D4);
+	CTL_DDR |= (1<<LCD_EN)|(1<<LCD_RW)|(1<<LCD_RS);
+
+	DATA_BUS = (0<<LCD_D7)|(0<<LCD_D6)|(1<<LCD_D5)|(0<<LCD_D4);
+	CTL_BUS|= (1<<LCD_EN)|(0<<LCD_RW)|(0<<LCD_RS);
+
+	_delay_ms(5);
+	CTL_BUS &=~(1<<LCD_EN);
+	_delay_ms(5);
+
+	lcd_send_command(LCD_CMD_4BIT_2ROW_5X7);
+	_delay_ms(5);
+	lcd_send_command(LCD_CMD_DISPLAY_CURSOR_BLINK);
+	_delay_ms(5);
+	lcd_send_command(0x80);
+
 }
-void LCD_WriteCMD(u8 cmd)
+
+/*
+*Function name 	: lcd_send_command
+*Parameters	    : uint8_t command
+*return		   		: void
+*purpose       	: sending a command to LCD by sending
+*				 		the first nibble then the second nibble
+*						enabling and disabling the LCD in between
+*/
+void lcd_send_command (uint8_t command)
 {
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, RS_PIN, GPIO_PIN_LOW);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, RW_PIN, GPIO_PIN_LOW);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN , GPIO_PIN_LOW);
-	
-	LCD_4_BIT_BUS = (cmd & 0xF0) | (LCD_4_BIT_BUS & 0x0F);
-	
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_HIGH);
-	_delay_ms(1);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_LOW);
-	
-	LCD_4_BIT_BUS = (cmd << 4) | (LCD_4_BIT_BUS & 0x0F);
-	
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_HIGH);
-	_delay_ms(1);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_LOW);	
-	
+	DATA_BUS=((command&0b11110000));
+	CTL_BUS &=~(1<<LCD_RS);
+	CTL_BUS |=(1<<LCD_EN);
+	_delay_ms(5);
+	CTL_BUS &=~(1<<LCD_EN);
+	_delay_ms(5);
+	DATA_BUS=((command&0b00001111)<<4);
+	CTL_BUS |=(1<<LCD_EN);
+	_delay_ms(5);
+	CTL_BUS &=~(1<<LCD_EN);
 	_delay_ms(5);
 }
-void LCD_WriteCHAR(u8 chr)
+
+/*
+*Function name 	: lcd_write_word
+*Parameters	    : uint8_t word[20]
+*return		   		: void
+*purpose       	: printing a full word to the
+*				 		LCD (Maximun 20 characters)
+*/
+void lcd_write_word(uint8_t word[20])
 {
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, RS_PIN, GPIO_PIN_HIGH);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, RW_PIN, GPIO_PIN_LOW);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN , GPIO_PIN_LOW);
-	
-	LCD_4_BIT_BUS = (chr & 0xF0) | (LCD_4_BIT_BUS & 0x0F);
-	
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_HIGH);
-	_delay_ms(1);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_LOW);
-	
-	LCD_4_BIT_BUS = (chr << 4) | (LCD_4_BIT_BUS & 0x0F);
-	
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_HIGH);
-	_delay_ms(1);
-	GPIO_SetPinVal(LCD_4_BIT_CMD_PORT, E_PIN, GPIO_PIN_LOW);
-	
-	_delay_ms(5);	
-}
-void LCD_WriteSTRING(u8 * str)
-{
-	u8 counter = 0;
-	while (str[counter] != '\0')
+	int i=0;
+	while(word[i]!='\0')
 	{
-		LCD_WriteCHAR(str[counter]);
-		counter++;
-	}
-	
-}
-void LCD_WriteINTEGER(u32 integer)
-{
-	u32 Revers_num = 0;
-	u8 displayed_num = 0;
-	u32 multiplier = 1, i = 0;
-	if(integer == 0)
-	{
-		LCD_WriteCHAR('0');
-	}
-	else if(integer < 0)
-	{
-		LCD_WriteCHAR('-');
-		integer *= -1;
-	}
-	
-	while (integer != 0)
-	{
-		Revers_num = (Revers_num * 10) + integer % 10;
-		if (Revers_num == 0)	
-		multiplier *= 10;
-		integer /= 10;
-	}	
-	while (Revers_num != i)
-	{
-		displayed_num = Revers_num % 10;
-		LCD_WriteCHAR(displayed_num + 48);
-		Revers_num/=10;
-		if (Revers_num == 0 && multiplier >= 10)
-		{
-			i = 1;
-			Revers_num = multiplier;
-		}	
+		lcd_write_character(word[i]);
+		i++;
 	}
 }
-void LCD_WriteDECIMAL(f32 decimal)
+
+/*
+*Function name 	: lcd_write_character
+*Parameters	    : uint8_t character
+*return		   		: void
+*purpose       	: sending one characterto LCD by sending
+*				 		 the first nibble first then the second nibble
+*						 enabling and disabling the LCD in between
+*/
+void lcd_write_character(uint8_t character)
 {
-   int val,i;
-   val = (int) decimal;
-   LCD_WriteINTEGER(val);
-   LCD_WriteCHAR('.');
-   decimal -= val;
-   for (i = 0; i < 5; i++)
-   {
-	   decimal *=10;
-	   val = (int)decimal;
-	   LCD_WriteINTEGER(val);
-	   decimal -=val;
-   }	
+
+	DATA_BUS=((character & 0b11110000));
+	CTL_BUS|=(1<<LCD_RS);
+	CTL_BUS |=(1<<LCD_EN);
+	_delay_ms(5);
+	CTL_BUS &=~(1<<LCD_EN);
+	_delay_ms(5);
+	DATA_BUS=((character & 0b00001111)<<4);
+	CTL_BUS |=(1<<LCD_EN);
+	_delay_ms(5);
+	CTL_BUS &=~(1<<LCD_EN);
+	_delay_ms(5);
 }
-void LCD_BurnCustomCHAR(u8 loc, u8 *arr)
+
+/*
+*Function name 	: lcd_clear
+*Parameters	    : void
+*return		   		: void
+*purpose       	: Clearing the lcd screen by sending
+*				 	the LCD_CMD_CLEAR_DISPLAY command to LCD
+*/
+void lcd_clear(void)
 {
-	int i;
-	LCD_WriteCMD(0X40 + (loc * 8));
-	for (i = 0; i < 8; i++)
-	{
-		LCD_WriteCHAR(arr[i]);
-	}
+	lcd_send_command(LCD_CMD_CLEAR_DISPLAY);
+	_delay_ms(5);
 }
-void LCD_ModifyDDRAM_CHAR(u8 loc, u8 *arr)
+void lcd_goto_xy (uint8_t line,uint8_t pos)				//line = 0 or 1
 {
-	int i;
-	
-}
-void LCD_GoTo(u8 row, u8 col)
-{
-	u8 arr[2] = {Cursor_Begin_1st_Line, Cursor_Begin_2st_Line};
-	LCD_WriteCMD(arr[row] + col);
-}
-void LCD_Clear(void)
-{
-	LCD_WriteCMD(Clear_Display_Screen);
+	lcd_send_command((0x80|(line<<6))+pos);
+	_delay_us (50);
 }
--- Autonomous_Car/Autonomous_Car/HAL/LCD/LCD.h
+++ Autonomous_Car/Autonomous_Car/HAL/LCD/LCD.h
@@ -1,45 +1,39 @@
 /*
  * LCD.h
  *
- * Created: 5/27/2022 4:41:34 PM
- *  Author: K.EMAM
+ * Author : Hossam Elbahrawy
  */
+
 #ifndef LCD_H_
 #define LCD_H_
 
-#include "LCD_CFG.h"
+#include <avr/io.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include "lcd_config.h"
 
-/*LCD Commands*/
 
-#define Clear_Display_Screen                 0x01
-#define Return_Home                          0x02
-#define Shift_Cursor_Left_Per_Read_Write     0x04
-#define Shift_cursor_Right_Per_Read_Write    0x06
-#define Shift_Display_Right_Per_Read_Write   0x05
-#define Shift_Display_Left_Per_Read_Write    0x07
-#define Display_Cursor_Blink_ALL_OFF         0x08
-#define Display_OFF_Cursor_ON                0x0A
-#define Display_ON_Cursor_OFF                0x0C
-#define Display_ON_Cursor_Blink              0x0E
-#define Shift_Cursor_Position_Left           0x10
-#define Shift_Cursor_Position_Right          0x14
-#define Shift_Entire_Display_Left            0x18
-#define Shift_Entire_Display_Right           0x1C
-#define Cursor_Begin_1st_Line                0x80
-#define Cursor_Begin_2st_Line                0xC0
-#define LCD_4_Bit_Mode                       0x28
-#define LCD_8_Bit_Mode                       0x38
+#define LCD_CMD_CLEAR_DISPLAY	             0x01
+#define LCD_CMD_CURSOR_HOME		             0x02
 
-void LCD_INIT(void);
-void LCD_WriteCMD(u8 cmd);
-void LCD_WriteCHAR(u8 chr);
-void LCD_WriteSTRING(u8 * str);
-void LCD_WriteINTEGER(u32 integer);
-void LCD_WriteDECIMAL(f32 decimal);
-void LCD_BurnCustomCHAR(u8 loc, u8 *arr);
-void LCD_ModifyDDRAM_CHAR(u8 loc, u8 *arr);
-void LCD_GoTo(u8 row, u8 col);
-void LCD_Clear(void);
+// Display control
+#define LCD_CMD_DISPLAY_OFF                0x08
+#define LCD_CMD_DISPLAY_NO_CURSOR          0x0c
+#define LCD_CMD_DISPLAY_CURSOR_NO_BLINK    0x0E
+#define LCD_CMD_DISPLAY_CURSOR_BLINK       0x0F
+
+// Function set
+#define LCD_CMD_4BIT_2ROW_5X7              0x28
+#define LCD_CMD_8BIT_2ROW_5X7              0x38
+
+//functions prototype
+void lcd_init(void);
+void lcd_send_command (uint8_t );
+void lcd_write_character(uint8_t );
+void lcd_write_word(uint8_t[]);
+void lcd_clear(void);
+void lcd_set_courser(uint8_t,uint8_t);
+void lcd_goto_xy (uint8_t , uint8_t );
 
 
 
